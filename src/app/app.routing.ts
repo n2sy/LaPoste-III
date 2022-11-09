4898e002125e0308fc03ac8035eeab912fe05d90
nidhal <nidhal.jelassi@fsegt.utm.tn>
2022-11-09 13:29:59 +0100

    add a routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,13 +4,26 @@ import { HomeComponent } from './home/home.component';
 import { ManageServersComponent } from './manage-servers/manage-servers.component';
 import { MsWordComponent } from './ms-word/ms-word.component';
 import { InfosComponent } from './infos/infos.component';
+import { AddComponent } from './add/add.component';
+import { UpdateComponent } from './update/update.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const myRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'cv', component: CvComponent },
-  { path: 'cv/:myid', component: InfosComponent },
+  {
+    path: 'cv',
+    children: [
+      { path: '', component: CvComponent },
+      { path: 'add', component: AddComponent },
+      { path: 'infos/:myid', component: InfosComponent },
+      { path: 'edit/:myid', component: UpdateComponent },
+      { path: '**', component: NotFoundComponent },
+    ],
+  },
   { path: 'servers', component: ManageServersComponent },
   { path: 'ms-word', component: MsWordComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found' },
 ];
 
 export const LAPOSTE_ROUTING = RouterModule.forRoot(myRoutes);
